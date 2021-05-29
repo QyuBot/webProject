96bb3e768d0fb50e96362c0e52ef151170680299
QyuBot <tkrlso123@gmail.com>
2021-05-29 14:48:58 +0900

    DB 구축에 필요한 파일과 SQL 추가, 템플릿 제거

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Title</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="">
+    <style>
+
+    </style>
+</head>
+<body scroll=no onload="init();">
+    <?php echo("<h1>hello</h1>");?>
+</body>
+</html>
+
+<script type="text/javascript">
+
+
+</script>
---
+++ settings/db_info.php
@@ -0,0 +1,9 @@
+<?php
+
+    $MYSQL_ADDRESS = "127.0.0.1";
+    
+    $MYSQL_USER_NAME = "";
+    $MYSQL_USER_PASSWORD = "";
+    $MYSQL_DATABASE_NAME = "";
+    
+?>
